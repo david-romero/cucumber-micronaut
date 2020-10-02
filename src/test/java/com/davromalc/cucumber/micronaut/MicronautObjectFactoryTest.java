8731b94482524e20233a648329c6b65d3ce60ad8
David Romero <davromalc@gmail.com>
2020-10-02 18:39:08 +0200

    Implemented Micronaut Object Factory for beans with dependencies
    Why: We want to integrate cucumber object factory with Micronaut Bean Registry

--- src/test/java/com/davromalc/cucumber/micronaut/MicronautObjectFactoryTest.java
+++ src/test/java/com/davromalc/cucumber/micronaut/MicronautObjectFactoryTest.java
@@ -1,6 +1,5 @@
 package com.davromalc.cucumber.micronaut;
 
-import io.cucumber.core.backend.ObjectFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,27 +10,40 @@ import org.junit.jupiter.api.Test;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class MicronautObjectFactoryTest {
 
-    MicronautObjectFactory objectFactory = new MicronautObjectFactory();
+  MicronautObjectFactory objectFactory = new MicronautObjectFactory();
 
-    @BeforeEach
-    void setUp() {
-        objectFactory.start();
-    }
+  @BeforeEach
+  void setUp() {
+    objectFactory.start();
+  }
 
-    @AfterEach
-    void tearDown() {
-        objectFactory.stop();
-    }
+  @AfterEach
+  void tearDown() {
+    objectFactory.stop();
+  }
 
-    @Test
-    void given_A_Bean_Registered_Into_Micronaut_Context_When_Cucumber_Gets_An_Instance_Then_The_Bean_Is_Returned() {
-        // given
-        objectFactory.applicationContext.registerSingleton(new MyBean());
+  @Test
+  void given_A_Bean_Registered_Into_Micronaut_Context_When_Cucumber_Gets_An_Instance_Then_The_Bean_Is_Returned() {
+    // given
+    objectFactory.applicationContext.registerSingleton(new MyBean());
 
-        // when
-        final MyBean myBean = objectFactory.getInstance(MyBean.class);
+    // when
+    final MyBean myBean = objectFactory.getInstance(MyBean.class);
 
-        // then
-        Assertions.assertNotNull(myBean);
-    }
+    // then
+    Assertions.assertNotNull(myBean);
+  }
+
+  @Test
+  void given_A_Bean_With_Dependencies_Registered_Into_Micronaut_Context_When_Cucumber_Gets_An_Instance_Then_The_Bean_Is_Returned() {
+    // given
+    objectFactory.applicationContext.registerSingleton(new MyBeanWithDependencies(new MyBean()));
+
+    // when
+    final MyBeanWithDependencies myBean = objectFactory.getInstance(MyBeanWithDependencies.class);
+
+    // then
+    Assertions.assertNotNull(myBean);
+    Assertions.assertDoesNotThrow(myBean::doStuff);
+  }
 }
---
+++ src/test/java/com/davromalc/cucumber/micronaut/MyBeanWithDependencies.java
@@ -0,0 +1,15 @@
+package com.davromalc.cucumber.micronaut;
+
+public class MyBeanWithDependencies {
+
+  private final MyBean myBean;
+
+  public MyBeanWithDependencies(MyBean myBean) {
+    this.myBean = myBean;
+  }
+
+  public void doStuff() {
+    myBean.doStuff();
+  }
+
+}
